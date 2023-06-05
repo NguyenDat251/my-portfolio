c4930e40346e8158f72988dc04164f8f7a2e688e
NguyenDat251 <nguyenquocdat2511998@gmail.com>
2023-06-05 17:56:16 +0700

    add icons and update css

--- src/components/SkillSection/SkillSection.js
+++ src/components/SkillSection/SkillSection.js
@@ -1,14 +1,15 @@
 import developerImg from "assets/developer.jpg";
+import classNames from "classnames";
 import "./SkillSection.css";
 
-const SkillTag = ({ skillName, x, y }) => {
+const SkillTag = ({ skillName, classes }) => {
   return (
     <div
-      className="bg-green-500 p-4 absolute text-white skillTag cursor-pointer"
-      style={{
-        top: y,
-        left: x,
-      }}
+      className={classNames(
+        "bg-green-500 p-4 absolute text-white skillTag cursor-pointer",
+        classes
+      )}
+      a
     >
       {skillName}
     </div>
@@ -17,22 +18,46 @@ const SkillTag = ({ skillName, x, y }) => {
 
 export default function SkillSection() {
   return (
-    <section className="flex justify-center items-center p-16 relative">
+    <section className="flex justify-center items-center p-16 relative h-screen">
       <div>
+        <h2>My Skills</h2>
         <img
-          className="margin"
+          className="margin w-screen lg:w-[400px] lg:h-[400px]"
           alt="developers"
           src={developerImg}
-          width={400}
-          height={400}
         />
       </div>
 
-      <SkillTag skillName={"HTML"} x={400} y={100} />
-      <SkillTag skillName={"Javascript"} x={500} y={300} />
-      <SkillTag skillName={"CSS"} x={300} y={200} />
-      <SkillTag skillName={"ReactJs"} x={1000} y={200} />
-      <SkillTag skillName={"TypeScript"} x={850} y={400} />
+      <SkillTag
+        skillName={"HTML"}
+        classes="top-[100px] left-[50px] lg:top-[400px] lg:left-[100px]"
+      />
+      <SkillTag
+        skillName={"Javascript"}
+        classes="top-[200px] left-[250px] lg:top-[500px] lg:left-[300px]"
+      />
+      <SkillTag
+        skillName={"CSS"}
+        classes="top-[80px] left-[250px] lg:top-[300px] lg:left-[200px]"
+      />
+      <SkillTag
+        skillName={"ReactJs"}
+        classes="top-[250px] left-[50px] lg:top-[1000px] lg:left-[200px]"
+      />
+      <SkillTag
+        skillName={"TypeScript"}
+        classes="top-[300px] left-[200px] lg:top-[850px] lg:left-[400px]"
+      />
+
+      <SkillTag
+        skillName={"Jest"}
+        classes="top-[300px] left-[200px] lg:top-[850px] lg:left-[400px]"
+      />
+
+      <SkillTag
+        skillName={"Cypress"}
+        classes="top-[300px] left-[200px] lg:top-[850px] lg:left-[400px]"
+      />
     </section>
   );
 }
