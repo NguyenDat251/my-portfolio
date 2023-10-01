15c6533b5ecf07eb28630504f0f83a96c807780f
NguyenDat251 <nguyenquocdat2511998@gmail.com>
2023-10-01 23:45:14 +0700

    fix: update default cv link

--- src/components/CVButton/CVButton.js
+++ src/components/CVButton/CVButton.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./CVButton.scss";
 
+const DEFAULT_CV_LINK =
+  "https://drive.google.com/file/d/1eEG1PpEWlFG_RHctqjrCxrz857o61b-7/view?usp=sharing";
+
 export default function CVButton() {
   //workaroud for process not defined
   //https://stackoverflow.com/a/70791920
-  const CV_LINK = process.env.REACT_APP_CV_LINK;
+  const CV_LINK = process.env.REACT_APP_CV_LINK || DEFAULT_CV_LINK;
 
   return (
     <>
